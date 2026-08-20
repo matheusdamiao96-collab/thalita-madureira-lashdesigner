2813903636398469b9ad9caa5b16fbac19e6f744
matheusdamiao96-collab <matheusdamiao96@gmail.com>
2026-08-19 22:30:06 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window.TM_CONFIG = {
+  SUPABASE_URL: "https://gbrrdzdqbtktptbtewxi.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_6FkJJlhcvQx9VDNDebrGHQ_dKa0PEvP",
+
+  BUSINESS_NAME: "Thalita Madureira Lash Designer",
+  BUSINESS_WHATSAPP: "5521968201354",
+  BUSINESS_INSTAGRAM: "https://www.instagram.com/thalita_madureira.lashdesigner/",
+  BUSINESS_ADDRESS: "Rua Professor Henrique Costa, 550 · Pechincha · Rio de Janeiro - RJ",
+  BUSINESS_TIMEZONE: "America/Sao_Paulo"
+};
